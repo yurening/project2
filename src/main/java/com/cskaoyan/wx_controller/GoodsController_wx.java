2d8fc5c31a113a0d4cbc732b3fb14bf32fab2207
qbj <you@example.com>
2019-11-21 15:29:33 +0800

    20191121-3

--- src/main/java/com/cskaoyan/wx_controller/GoodsController_wx.java
+++ src/main/java/com/cskaoyan/wx_controller/GoodsController_wx.java
@@ -2,8 +2,10 @@ package com.cskaoyan.wx_controller;
 
 import com.cskaoyan.bean.goods.Brand;
 import com.cskaoyan.bean.goods.ResponseType;
+import com.cskaoyan.bean.user.User;
 import com.cskaoyan.mapper.BrandMapper;
 import com.cskaoyan.service.GoodsService;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,6 +66,8 @@ public class GoodsController_wx {
 
     @RequestMapping("goods/detail")
     public ResponseType goodsDetail(Integer id){
+        User principal = (User) SecurityUtils.getSubject().getPrincipal();
+        System.out.println(principal.getId());
         ResponseType goodsAndAllById = goodsService.getGoodsAndAllById(id);
         return goodsAndAllById;
     }
